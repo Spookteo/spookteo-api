eec2029b2abcd6d3b265196cd572f57e11ed2a58
Anthony <quereantho@gmail.com>
2021-03-20 14:57:57 +0100

    Add record-db

---
+++ src/data-access/record-db.ts
@@ -0,0 +1,51 @@
+import { Db } from "mongodb";
+import { RecordInfos } from "../types";
+
+
+interface GetRecordsOptions {
+
+}
+
+const RECORD_COLLECTION = 'records';
+
+interface MakeRecordDbOptions {
+    makeDb: () => Promise<Db>;
+    collection: string;
+}
+
+export default function makeRecordDb({makeDb, collection}: MakeRecordDbOptions) {
+
+    return Object.freeze({
+        saveRecord,
+        getRecords
+    })
+    
+
+    /**
+     * This function save a record in the database
+     * @param recordsInfos informations to save
+     */
+    async function saveRecord(recordsInfos: RecordInfos) {
+        const db = await makeDb();
+
+        const result = await db
+            .collection(collection)
+            .insertOne(recordsInfos);
+            
+        const insertedInfos = result.ops[0];
+        return insertedInfos;
+    }
+
+    /**
+     * This function return all records from the database
+     * @param options Options passed
+     */
+    async function getRecords({}: GetRecordsOptions) {
+        const db = await makeDb()
+        const result = await db 
+            .collection(collection)
+            .find();
+        
+        return await result.toArray();
+    }
+}
