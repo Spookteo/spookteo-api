c4b94972c333c68b3387279def4f3b198ed0d837
Anthony <quereantho@gmail.com>
2021-05-10 14:59:51 +0200

    Add port as env

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import 'module-alias/register';
+import { PORT } from '@shared/environment';
 import app from "./src";
 
 app.isReady.then(() => {
-const server = app.listen(8080, () => {
+const server = app.listen(PORT, () => {
     // @ts-ignore
     const port = server.address().port;
     console.log("App running on port", port);
--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -20,3 +20,4 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 export const DATABASE_NAME = process.env.DATABASE_NAME;
 export const DATABASE_CERT_LOCATION = process.env.DATABASE_CERT_LOCATION;
 export const DATABASE_CERT = process.env.DATABASE_CERT;
+export const PORT = process.env.PORT ?? 8080;
